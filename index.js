99a916bec8396e7a79c23df6317eee84b24a8b01
David Silva <ti@mkplace.com.br>
2018-05-18 16:49:28 -0300

    2.0.1

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ class catalog {
         require('./strategies');
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
+
+        this.app.all('*', function(req, res, next) {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "*");
+            next();
+        });
     }
 
     auth(endpoint, token) {
